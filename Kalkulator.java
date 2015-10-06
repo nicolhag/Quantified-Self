b84a99de7972bf269d4dfb6f7e0092287fe6c3c7
Nicolai Hagen <nicolhag@ifi.uio.no>
2015-10-06 13:34:07 +0200

    first commit

---
+++ Kalkulator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.File;
+
+class Kalkulator{
+    public static void main(String[] args) throws Exception {
+        DeltakerBehandler d = new DeltakerBehandler();
+    }
+}
+
+class DeltakerBehandler{
+    HashMap<Deltaker> deltakere = new HashMap<Deltaker>();
+
+    private void lesInnFraFil(){
+        Scanner leser = new Scanner(new File("sample_fysisk.csv"));
+
+        while (leser.hasNextLine()){
+            String innlest = leser.nextLine();
+            String[] parsed = innlest.split(",");
+
+            System.out.println("*********************");
+            System.out.println("*********************");
+
+            String timestamp = parsed[0];
+            String navn = parsed[1];
+            String antallSkritt = parsed[2];
+            String antalltimerSovn = parsed[3];
+
+            System.out.println("Navn: " + navn);
+            System.out.println("Timestamp: " + timestamp);
+            System.out.println("Antall skritt: " + antallSkritt);
+            System.out.println("Antall timer søvn: " + antalltimerSovn);
+
+            System.out.println("*********************");
+            System.out.println("*********************");
+
+        }
+    }
+}
+
+class Deltaker{
+    String
+}
